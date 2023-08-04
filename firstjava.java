673ce9bafbf5f7a96cbbe24091bba1e12dbf2b3a
Ubuntu <azureuser@overlord.mb1hr4drtwbepe0wj245pjwjag.bx.internal.cloudapp.net>
2023-08-04 04:50:18 +0000

    a

--- firstjava.java
+++ firstjava.java
@@ -1,6 +1,6 @@
 public class firstjava
 {
         public static void main(String[] args) {
-                System.out.println("Hello welcome to jenkins");
+                System.out.println("Hello welcome to jenkins! demonstarta free style project ");
         }
 }
